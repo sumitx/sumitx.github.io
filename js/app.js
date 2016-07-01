e4e7ec7a824684bd39a74d24b98f2018f1d5745b
Sumit Pal <sumit_iths@yahoo.com>
2016-07-01 12:57:50 -0400

    adding more styling

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@ $content = $('#content');
 var weatherAPIKey = "09d7f1e96703344071bc75edb08f31c8";
 var zipCode = 11377;
 
-
-
 function getWeather() {
 	sourceURL = "http://api.openweathermap.org/data/2.5/forecast/weather?zip=" + zipCode + ",us&units=metric&APPID=" + weatherAPIKey
 
@@ -28,6 +26,9 @@ function handleResponse(data) {
 	$content.append("<h1>Here's the weather for " + data.city.name + "<br></h1>");
 
 	$content.append("<h2>Temperature: " + tempInF + "</h2><br>");
+
+	toggleBackground(tempInF);
+
 	conditions =  data.list[0].weather[0].description;
 	$content.append("<h4>Conditions: " + conditions + "</h4><br>");
 	conditions = conditions.split(' ').join('+')
@@ -35,6 +36,18 @@ function handleResponse(data) {
 	//now do the gif interaction
 	handleGif(conditions);
 
+}
+
+function toggleBackground(tempRange) {
+	if (tempRange < 33) {
+		$('.content-section-b').attr('id', 'temperature-background--freezing');
+	} else if (tempRange > 33 && tempRange < 51) {
+		$('.content-section-b').attr('id', 'temperature-background--cold');
+	} else if (tempRange > 51 && tempRange < 76) {
+		$('.content-section-b').attr('id', 'temperature-background--warm');
+	} else if (tempRange > 76) {
+		$('.content-section-b').attr('id', 'temperature-background--hot');
+	}
 }
 
 
@@ -65,9 +78,9 @@ function refreshGif(){
 
 		var rand = Math.floor((Math.random() * maxLength) + 1);
 
-		$('#image').empty();
-
-		$('#image').append("<div id='image'><img src=" + data.data[rand].images.downsized.url + "></div>");
+		$('#image')
+			.empty()
+			.append("<div id='image'><img src=" + data.data[rand].images.downsized.url + "></div>");
 
 	});
 }
